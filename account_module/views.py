a4f28b3ce1ea509889a7efa3cbb399311a3414ef
Brbodd <b.z.10231384@gmail.com>
2026-08-16 20:14:52 +0330

    feat: make first migration, setup admin pannel, styling CSS struc.

--- account_module/views.py
+++ account_module/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from reservation_module.models import Reservation
 
 
 class AccountView(TemplateView):
     template_name = "account_module/account.html"
+    model = Reservation
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["reservations"] = f"this is my reserve"
+        return context
+    
 
 
 class RegisterView(TemplateView):
