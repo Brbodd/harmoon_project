1b2a916a386dc668913072f0317eb99e84194b8a
Brbodd <b.z.10231384@gmail.com>
2026-08-29 15:49:26 +0330

    fix sms otp

--- account_module/views.py
+++ account_module/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from reservation_module.models import Reservation
 from .forms import RegisterForm, LoginForm, PhoneNumberVerificationForm
 from .models import User, PhoneVerification
-from .services import sms_verify
+from .services import send_otp
 
 
 
@@ -127,7 +127,7 @@ class RegisterView(View):
                 request.session["register_phone_number"] = user_phone_number
                 request.session["register_full_name"] = user_full_name
 
-                sms_verify(user_phone_number)
+                send_otp(user_phone_number)
 
                 return redirect(
                     reverse("phone-verify-page")
